be8310a0f4722497e5dbe492e48dd0703413042c
Kenan Poole <kpoole@wakeindustrial.com>
2023-08-17 16:00:30 -0400

    Mess around with grid

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,27 @@
 function App() {
 
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <p>Testing</p>
+    <div className="min-h-screen bg-gray-100">
+      <div className="bg-white border-b border-gray-100">
+        <div className="grid grid-cols-2 gap-4">
+          <div className="row-span-2 bg-red-100 text-right align-bottom">
+            1
+          </div>
+        
+          <h1 className="text-3xl font-bold">Kenan Poole</h1>
+          <h2 className="text-xl">Raleigh, NC</h2>
+ 
+          
+         
+        </div>
+      </div>
+    <main>
+      <div className="rounded-md ring-1 ring-black ring-opacity-5 py-1 bg-white">
+        <p>Hello world!</p>
+      </div>
+    </main>
+      
+      
     </div>
   )
 }
