16c8247d6a166939bfaf952ac300c2443219b463
plector <kenanpoole@gmail.com>
2023-08-22 07:18:51 -0400

    Fix grid columns

--- src/App.tsx
+++ src/App.tsx
@@ -1,139 +1,139 @@
 import LycaonImg from './images/lycaon.png'
 
 function App() {
-    const Lycaon = LycaonImg;
+  const Lycaon = LycaonImg;
 
-return (
-<div className="min-h-screen bg-gray-100">
-    <div className="relative lg:flex lg:items-start">
+  return (
+    <div className="min-h-screen bg-white">
+      <div className="relative lg:flex lg:items-start">
 
-    
-    <aside id="sidebar"
-        className="min-h-screen hidden fixed top-0 bottom-0 left-0 z-20 h-full w-16 bg-gradient-to-b from-white to-gray-100 transition-all duration-300 overflow-hidden lg:sticky lg:w-80 lg:shrink-0 lg:flex lg:flex-col lg:justify-end lg:items-end 2xl:max-w-lg 2xl:w-full dark:from-dark-800 dark:to-dark-700">
-        <div className="relative min-h-0 flex-1 flex flex-col xl:w-80">
+
+        <aside id="sidebar"
+          className="min-h-screen hidden fixed top-0 bottom-0 left-0 z-20 h-full w-16 bg-gradient-to-b from-gray-100 to-white transition-all duration-300 overflow-hidden lg:sticky lg:w-80 lg:shrink-0 lg:flex lg:flex-col lg:justify-end lg:items-end 2xl:max-w-lg 2xl:w-full dark:from-dark-800 dark:to-dark-700">
+          <div className="relative min-h-0 flex-1 flex flex-col xl:w-80">
             <a href="/" className="flex items-center pt-8 px-4 lg:px-8 xl:px-16">
-                <h1 className="hidden grow lg:block text-blue-500 text-3xl font-medium text-right">Kenan Poole</h1>
+              <h1 className="hidden grow lg:block text-blue-500 text-3xl font-medium text-right">Kenan Poole</h1>
             </a>
             <div className="overflow-y-auto overflow-x-hidden px-4 lg:overflow-hidden lg:px-8 xl:px-16">
-                <nav id="indexed-nav" className="hidden lg:block lg:mt-4">
-                    <div>
-                        <ul>
-                            <li className="py-2.5">
-                                <h2 className="text-right font-medium text-lg"><a id="introduction" href="#introduction"
-                                        className="heading-permalink" title="Permalink">Introduction</a></h2>
-                            </li>
-                            <li className="py-2.5">
-                                <h2 className="text-right font-medium text-lg"><a id="experience" href="#experience"
-                                        className="heading-permalink" title="Permalink">Experience</a></h2>
-                            </li>
-                            <li className="py-2.5">
-                                <h2 className="text-right font-medium text-lg"><a id="projects" href="#projects"
-                                        className="heading-permalink" title="Permalink">Projects</a></h2>
-                            </li>
-                            <li className="py-2.5">
-                                <h2 className="text-right font-medium text-lg"><a id="web-art" href="#web-art"
-                                        className="heading-permalink" title="Permalink">Web Art</a></h2>
-                            </li>
-                            <li className="py-2.5">
-                                <h2 className="text-right font-medium text-lg"><a id="other-projects" href="#other-projects"
-                                        className="heading-permalink" title="Permalink">Other Projects</a></h2>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
+              <nav id="indexed-nav" className="hidden lg:block lg:mt-4">
+                <div>
+                  <ul>
+                    <li className="py-2.5">
+                      <h2 className="text-right font-medium text-lg"><a id="introduction" href="#introduction"
+                        className="heading-permalink" title="Permalink">Introduction</a></h2>
+                    </li>
+                    <li className="py-2.5">
+                      <h2 className="text-right font-medium text-lg"><a id="experience" href="#experience"
+                        className="heading-permalink" title="Permalink">Experience</a></h2>
+                    </li>
+                    <li className="py-2.5">
+                      <h2 className="text-right font-medium text-lg"><a id="projects" href="#projects"
+                        className="heading-permalink" title="Permalink">Projects</a></h2>
+                    </li>
+                    <li className="py-2.5">
+                      <h2 className="text-right font-medium text-lg"><a id="web-art" href="#web-art"
+                        className="heading-permalink" title="Permalink">Web Art</a></h2>
+                    </li>
+                    <li className="py-2.5">
+                      <h2 className="text-right font-medium text-lg"><a id="other-projects" href="#other-projects"
+                        className="heading-permalink" title="Permalink">Other Projects</a></h2>
+                    </li>
+                  </ul>
+                </div>
+              </nav>
             </div>
             <div className="flex-grow flex flex-col justify-end">
             </div>
-        </div>
-    </aside>
+          </div>
+        </aside>
 
-    <section className="flex-1">
-        <div className="px-8 sm:px-16 lg:px-24 mt-8 sm:mt-16 lg:mt-2 grid-cols-3">
-            <div>
-        <article className="overflow-hidden rounded-lg border border-gray-100 bg-white shadow-lg">
-            <img alt="A Medium Metamorphosis | Image"
-                src={Lycaon}
-                className="h-56 w-full object-cover" />
+        <section className="flex-1 mt-8">
+          <div className="px-8 sm:px-16 lg:px-24 mt-8 sm:mt-16 lg:mt-2 grid grid-cols-3 gap-8">
+            <div className="overflow-hidden rounded-lg border border-gray-100 bg-white shadow-lg hover:shadow-xl">
+              <a href="#">
+                <img alt="A Medium Metamorphosis | Image"
+                  src={Lycaon}
+                  className="h-56 w-full object-cover" />
 
-            <div className="p-4 sm:p-6">
-                <a href="#">
+                <div className="p-4 sm:p-6">
+                  <a href="#">
                     <h3 className="text-lg font-medium text-gray-900">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     </h3>
-                </a>
+                  </a>
 
-                <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
+                  <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae
                     dolores, possimus pariatur animi temporibus nesciunt praesentium dolore
                     sed nulla ipsum eveniet corporis quidem, mollitia itaque minus soluta,
                     voluptates neque explicabo tempora nisi culpa eius atque dignissimos.
                     Molestias explicabo corporis voluptatem?
-                </p>
+                  </p>
 
-                <a href="#" className="group mt-4 inline-flex items-center gap-1 text-sm font-medium text-blue-600">
+                  <a href="#" className="group mt-4 inline-flex items-center gap-1 text-sm font-medium text-blue-600">
                     Find out more
-                </a>
+                  </a>
+                </div>
+              </a>
             </div>
-        </article>
-        </div>
-        <div>
-        <article className="overflow-hidden rounded-lg border border-gray-100 bg-white shadow-lg">
-            <img alt="A Medium Metamorphosis | Image"
-                src={Lycaon}
-                className="h-56 w-full object-cover" />
+            <div className="overflow-hidden rounded-lg border border-gray-100 bg-white shadow-lg">
+              <a href="#">
+                <img alt="A Medium Metamorphosis | Image"
+                  src={Lycaon}
+                  className="h-56 w-full object-cover" />
 
-            <div className="p-4 sm:p-6">
-                <a href="#">
+                <div className="p-4 sm:p-6">
+                  <a href="#">
                     <h3 className="text-lg font-medium text-gray-900">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     </h3>
-                </a>
+                  </a>
 
-                <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
+                  <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae
                     dolores, possimus pariatur animi temporibus nesciunt praesentium dolore
                     sed nulla ipsum eveniet corporis quidem, mollitia itaque minus soluta,
                     voluptates neque explicabo tempora nisi culpa eius atque dignissimos.
                     Molestias explicabo corporis voluptatem?
-                </p>
+                  </p>
 
-                <a href="#" className="group mt-4 inline-flex items-center gap-1 text-sm font-medium text-blue-600">
+                  <a href="#" className="group mt-4 inline-flex items-center gap-1 text-sm font-medium text-blue-600">
                     Find out more
-                </a>
+                  </a>
+                </div>
+              </a>
             </div>
-        </article>
-        </div>
-        <div>
-        <article className="overflow-hidden rounded-lg border border-gray-100 bg-white shadow-lg">
-            <img alt="A Medium Metamorphosis | Image"
-                src={Lycaon}
-                className="h-56 w-full object-cover" />
+            <div className="overflow-hidden rounded-lg border border-gray-100 bg-white shadow-lg">
+              <a href="#">
+                <img alt="A Medium Metamorphosis | Image"
+                  src={Lycaon}
+                  className="h-56 w-full object-cover" />
 
-            <div className="p-4 sm:p-6">
-                <a href="#">
+                <div className="p-4 sm:p-6">
+                  <a href="#">
                     <h3 className="text-lg font-medium text-gray-900">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     </h3>
-                </a>
+                  </a>
 
-                <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
+                  <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae
                     dolores, possimus pariatur animi temporibus nesciunt praesentium dolore
                     sed nulla ipsum eveniet corporis quidem, mollitia itaque minus soluta,
                     voluptates neque explicabo tempora nisi culpa eius atque dignissimos.
                     Molestias explicabo corporis voluptatem?
-                </p>
+                  </p>
 
-                <a href="#" className="group mt-4 inline-flex items-center gap-1 text-sm font-medium text-blue-600">
+                  <a href="#" className="group mt-4 inline-flex items-center gap-1 text-sm font-medium text-blue-600">
                     Find out more
-                </a>
+                  </a>
+                </div>
+              </a>
             </div>
-        </article>
-        </div>
-        </div>
-    </section>
-    </div>
-    {/* <header>
+          </div>
+        </section>
+      </div>
+      {/* <header>
         <div className="bg-white mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
             <div className="sm:flex sm:items-center sm:justify-between">
                 <div className="text-center sm:text-left">
@@ -189,8 +189,8 @@ return (
     </main> */}
 
 
-</div>
-)
+    </div>
+  )
 }
 
 export default App
